aaa7e454976cfe464d15fce059545db1246c4331
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-03-11 18:06:02 +0600

    explore cookies & alert, confirm, prompt method

--- Interact with Browser/local-storage.js
+++ Interact with Browser/local-storage.js
@@ -1,6 +1,22 @@
-const hello = localStorage.getItem('hello');
-console.log(hello);
+// const hello = localStorage.getItem('hello');
+// console.log(hello);
+
+const setName = () => {
+    localStorage.setItem("name", "Sakib Khan");
+    console.log(localStorage);
+}
 
 const setAge = () => {
-    window.localStorage.setItem('age', 31);
+    window.localStorage.setItem("age", 40);
 }
+
+
+
+// const displayItem = () => {
+//     const ul = document.getElementById('item-container');
+//     const li = document.createElement('li');
+//     li.innerText = `${setName()}`;
+//     ul.appendChild(li);
+// }
+
+// displayItem();
--- Interact with Browser/prompt.js
+++ Interact with Browser/prompt.js
@@ -26,3 +26,38 @@ const getInfo = () => {
         console.log('Welcome to this world!');
     }
 }
+
+/** ১. prompt দিয়ে ইউজারের কাছ থেকে একটা সংখ্যা নাও। সেটার সাথে ২০০যোগ করো। 
+যোগ করতে গেলে parse করা লাগলে সেটা করো। তারপর সেই রেজাল্টকে alert দিয়ে 
+আউটপুট দেখাও 
+*/
+const doSum = () => {
+    const input = prompt("Write a Number");
+    if (input === null) {
+        alert('Invalid input');
+    } else {
+        const inputParse = parseInt(input);
+        const sum = inputParse + 200;
+        alert("Result is : " + sum);
+    }
+}
+
+/** 
+২. ব্রাউজারে একটা confirm করে জিজ্ঞেস করো তুমি কি তোমার ওয়েবসাইট এর লোকেশন 
+দেখতে চাও। যদি বলে দেখতে চাই তাহলে সেই ওয়েবসাইট এর লোকেশন এর href জিনিসটা 
+কনসোল লগ করে দেখাও 
+*/
+const showLocation = () => {
+    const msg = confirm('Are you want to show your location');
+    if (msg === true) {
+        console.log(window.location.href);
+    } else {
+        console.log('Thak dekis na');
+    }
+}
+
+/** 
+৩. Cookies কি জিনিস। এইটা দিয়ে কি করা হয়। সেটা ইংরেজিতে ৫ থেকে ৭ লাইনের মধ্যে 
+লিখে ফেলো। 
+*/
+
